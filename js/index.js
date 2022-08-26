815c28caf67e4d0852b7b2a0cc59d5d034276aac
Pratham Sitoula <psitoula2003@gmail.com>
2022-08-26 15:57:01 -0600

    Implemented sticky nav

--- js/index.js
+++ js/index.js
@@ -10,3 +10,25 @@ window.addEventListener("load", () => {
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+const header = document.getElementById("header");
+const nav = document.querySelector(".nav");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent.isIntersecting);
+    if (ent.isIntersecting == false) {
+      nav.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting == true) {
+      nav.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-40px",
+  }
+);
+obs.observe(header);
